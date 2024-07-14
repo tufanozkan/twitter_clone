fed68517d5f867e18a715d242b5af1534810b307
tufan özkan <tuf.ozkan@gmail.com>
2024-07-14 21:13:10 +0300

    client fixed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors())
+app.use(express.json());
+
 
 const db = require("./models")
+
+const postsRouter = require("./routes/Posts")
+app.use("/posts",postsRouter)
 
 db.sequelize.sync().then(()=>{
     app.listen(3001, ()=>{
